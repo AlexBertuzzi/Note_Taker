b6b2c74866e69904f26342cb5bc098037a1a9de1
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-01-12 15:58:08 -0600

    setting up get requests

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require("express");
 var path = require("path");
 var  fs = require("fs");
+var noteData = require("/db/db.json")
 var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+app.get("/api/notes", function(req, res) {
+    res.json(noteData)
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
