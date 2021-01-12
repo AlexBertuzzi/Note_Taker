95ce53f299862f5b64e3b028984e3eded2835cd8
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-01-12 17:33:04 -0600

    setting up heroku

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var path = require("path");
 var  fs = require("fs");
 var noteList = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
 app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
