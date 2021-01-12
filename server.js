54040d7dbb8c7f41ca9c4c98c0f68ed76e31b8f4
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-01-12 17:11:06 -0600

    stetting up get and post

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 var express = require("express");
 var path = require("path");
 var  fs = require("fs");
-var noteData = require("/db/db.json")
+var noteList = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
 var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
 app.get("/api/notes", function(req, res) {
-    res.json(noteData)
+    return res.json(noteList);
+});
+
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    notelist.push(newNote);
+    return res.json(noteList);
 });
 
 app.listen(PORT, function() {
